197b34d342269155afbaffbfdf5ea3f68e651989
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-11-27 17:35:56 +0500

    add aws-cdk stater template

---
+++ aws-cdk-step-01/test/aws-cdk-step-01.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkStep01 from '../lib/aws-cdk-step-01-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-step-01-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkStep01.AwsCdkStep01Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
